a19d976577f9c1b27513928a0d057e87a9415b11
Your Name <you@example.com>
2022-05-19 17:34:52 +0200

    biblioalexandriaannotations first

---
+++ eclipse-workspace/BiblioAlexandriaAnnotations/src/entities/Cliente.java
@@ -0,0 +1,39 @@
+package entities;
+
+import org.springframework.stereotype.Component;
+
+@Component("BeanCliente")
+public class Cliente extends Persona {
+	
+	private int idCliente;
+	
+	public String chiediLibro(Libro libro) {
+		return getNome() + " ha chiesto il libro " + libro.getTitolo();
+	}
+	
+	public String restituisciLibro(Libro libro) {
+		return getNome() + " ha restituito il libro " + libro.getTitolo();
+	}
+
+	public Cliente() {
+		
+	}
+	
+	public Cliente(int idCliente) {
+		super();
+		this.idCliente = idCliente;
+	}
+
+	public int getIdCliente() {
+		return idCliente;
+	}
+
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	
+	
+	
+	
+
+}
